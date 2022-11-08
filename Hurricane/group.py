52c1f9e2933f3738adbbd74a06b90724b0c9124f
PeanutbutterWarrior <50717143+PeanutbutterWarrior@users.noreply.github.com>
2022-11-08 08:49:44 +0000

    Make looped groups work

--- Hurricane/group.py
+++ Hurricane/group.py
@@ -21,7 +21,18 @@ class Group:
         return item in self._members
 
     async def send(self, message: Any):
-        await asyncio.gather(member.send(message) for member in self._members)
+        await self.checked_send(message, set())
+
+    async def checked_send(self, message: Any, already_sent_to: set):
+        new_already_sent_to = already_sent_to.copy()
+        for member in self._members:
+            new_already_sent_to.add(member)
+
+        await asyncio.gather(
+            member.checked_send(message, new_already_sent_to)
+            for member in self._members
+            if member not in already_sent_to
+        )
 
     def add(self, new_member: Group | Client):
         self._members.add(new_member)

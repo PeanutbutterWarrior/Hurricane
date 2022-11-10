35deb03efe5798083fce8c70e2cea3e098b60e41
PeanutbutterWarrior <50717143+PeanutbutterWarrior@users.noreply.github.com>
2022-11-10 12:11:24 +0000

    Move examples into folder

--- example_client.py
+++
@@ -1,9 +0,0 @@
-import socket
-from Hurricane.client_functions import ServerConnection
-
-while True:
-    with ServerConnection('localhost', 65432, socket.AF_INET, socket.SOCK_STREAM) as s:
-        while True:
-            message = input("> ")
-            s.send(message)
-            print(f"Received {s.recv()}")

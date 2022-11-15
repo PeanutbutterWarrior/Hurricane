1d67c2a1042eebe773953aee9ecabb5de1b869d4
PeanutbutterWarrior <50717143+PeanutbutterWarrior@users.noreply.github.com>
2022-11-15 04:30:09 +0000

    Add rooms

--- Examples/Chat Server/chat_server.py
+++ Examples/Chat Server/chat_server.py
@@ -4,24 +4,44 @@ server = Hurricane.Server(timeout=5)
 
 clients = []
 names = {}
+containing_groups = {}
+
+rooms = {'all': Hurricane.Group()}
 
 
 @server.on_new_connection
 async def on_new_client(new_client: Hurricane.Client):
     clients.append(new_client)
+    rooms['all'].add(new_client)
+    containing_groups[new_client] = 'all'
     name = (await new_client.receive()).contents
     names[new_client] = name.title()
     print(f"{name} has joined the chat")
-    for client in clients:
-        await client.send(f"{name} has joined the chat")
+    await rooms['all'].send(f"{name} has joined the chat")
 
 
 @server.on_receiving_message
 async def got_message(message: Hurricane.Message):
-    formatted_message = f"{names[message.author]}: {message.contents}"
-    print(formatted_message)
-    for client in clients:
-        await client.send(formatted_message)
+    if message.contents[:6] == "/join ":
+        group_name = message.contents[6:]
+        if group_name not in rooms:
+            rooms[group_name] = Hurricane.Group()
+        old_group_name = containing_groups[message.author]
+
+        rooms[old_group_name].remove(message.author)
+
+        await rooms[old_group_name].send(f"{names[message.author]} has changed to group {group_name}")
+        await rooms[group_name].send(f"{names[message.author]} has joined group {group_name}")
+        await message.author.send(f"You have changed to group {group_name}")
+
+        rooms[group_name].add(message.author)
+        containing_groups[message.author] = group_name
+        print(f"{names[message.author]} changed from group {old_group_name} to {group_name}")
+    else:
+        formatted_message = f"{names[message.author]}: {message.contents}"
+        print(formatted_message)
+        group_name = containing_groups[message.author]
+        await rooms[group_name].send(formatted_message)
 
 
 @server.on_client_disconnect
